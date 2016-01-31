4523fcc26ce45a4fb7a37e98c03829feb31fb3bf
Tito Lins <tito@blinx.com.br>
2016-01-31 00:21:47 -0200

    added category and factory to package file

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -1,9 +1,13 @@
 from project.models.user import User, UserFactory
+from project.models.category import Category, CategoryFactory
 
+## users
 uf = UserFactory()
-
 admin = uf.getAdminUser()
-
 users = [uf.getRegularUser() for i in range(5)]
-
 user = users[0]
+
+
+## categories
+cf = CategoryFactory()
+categories = cf.getCategories()

ddb2b96b3d5959d950aa3f8567244ae01f87c67c
Tito Lins <tito@blinx.com.br>
2016-02-01 03:52:49 -0200

    divided picture attr into thumbnail and header_picture

--- project/models/category.py
+++ project/models/category.py
@@ -25,18 +25,25 @@ class CategoryFactory(object):
                 'Toalha de mesa': 7,
                 'Acessorio': 3,
         }
-        self.picture = "http://placehold.it/400x300"
+        self.thumbnail = "http://placehold.it/400x300"
+        self.header_picture = "http://placehold.it/1400x400"
 
     def getCategories(self):
-        return [Category(c_id=v, name=k, picture=self.picture) \
+        return [
+                Category(
+                    c_id = v,
+                    name = k,
+                    thumbnail = self.thumbnail,
+                    header_picture = self.header_picture) \
                 for k,v in self.names.items()]
 
 
 class Category(object):
-    def __init__(self, c_id, name, picture):
+    def __init__(self, c_id, name, thumbnail, header_picture):
         self.id_ = c_id
         self.name = name
-        self.picture = picture
+        self.thumbnail = thumbnail
+        self.header_picture = header_picture
 
 if __name__ == '__main__':
     cf = CategoryFactory()

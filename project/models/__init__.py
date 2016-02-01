1d543393482958c5ef6f3ea112996896418e1315
Tito Lins <tito@blinx.com.br>
2016-02-01 03:10:15 -0200

    added products to package (for use during development)

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -1,5 +1,6 @@
 from project.models.user import User, UserFactory
 from project.models.category import Category, CategoryFactory
+from project.models.product import Product, ProductFactory
 
 ## users
 uf = UserFactory()
@@ -7,7 +8,10 @@ admin = uf.getAdminUser()
 users = [uf.getRegularUser() for i in range(5)]
 user = users[0]
 
-
 ## categories
 cf = CategoryFactory()
 categories = cf.getCategories()
+
+## products
+pf = ProductFactory()
+products = pf.getProducts()
